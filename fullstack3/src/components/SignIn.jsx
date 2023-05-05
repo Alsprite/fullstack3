9bf0b3ad493994fcea606bfa91c16bf7acf0b3b3
Alsprite <henri.mykra@esedulainen.fi>
2023-05-05 10:38:39 +0300

    10.8.

--- fullstack3/src/components/SignIn.jsx
+++ fullstack3/src/components/SignIn.jsx
@@ -1,7 +1,43 @@
-import { Text } from 'react-native';
+import { Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { Formik } from 'formik';
+import theme from '../theme';
 
-const SignIn = () => {
-  return <Text>The sign-in view</Text>
+const initialValues = {
+    username: '',
+    password: ''
+}
+
+const SignIn = () => { 
+
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <View style={theme.container}>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={onSubmit}
+      >
+        {({ handleChange, handleSubmit, values }) => (
+          <>
+            <TextInput
+              placeholder="Username"
+              onChangeText={handleChange("username")}
+              value={values.username}
+            />
+            <TextInput
+              placeholder="Password"
+              onChangeText={handleChange("password")}
+              value={values.password}
+              secureTextEntry
+            />
+            <TouchableOpacity onPress={handleSubmit}><Text>Sign in</Text></TouchableOpacity>
+          </>
+        )}
+      </Formik>
+    </View>
+  );
 };
 
 export default SignIn;
