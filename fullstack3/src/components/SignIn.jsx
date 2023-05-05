d0c9620a3dadda6783729297f61a68a423b64baa
Alsprite <henri.mykra@esedulainen.fi>
2023-05-05 10:54:46 +0300

    10.9.

--- fullstack3/src/components/SignIn.jsx
+++ fullstack3/src/components/SignIn.jsx
@@ -1,24 +1,39 @@
 import { Text, TextInput, TouchableOpacity, View, StyleSheet } from 'react-native';
 import { Formik } from 'formik';
 import theme from '../theme';
-import FormikTextInput from './FormikTextInput';
+import * as yup from 'yup';
 
 const styles = StyleSheet.create({
     input: {
-      width: 400,
-      marginVertical: 10,
-      paddingHorizontal: 5,
-      paddingVertical: 10,
-      borderWidth: 0.5,
-      borderRadius: 5,
-      borderColor: 'grey',
+        width: 400,
+        marginVertical: 10,
+        paddingHorizontal: 5,
+        paddingVertical: 10,
+        borderWidth: 0.5,
+        borderRadius: 5,
+        borderColor: 'grey',
     },
     button: {
-      marginVertical: 10,
-      paddingHorizontal: 5,
-      paddingVertical: 10,
+        marginVertical: 10,
+        paddingHorizontal: 5,
+        paddingVertical: 10,
     },
+    errorText: {
+        color: 'red',
+        marginBottom: 5,
+    }
   });
+
+const validationSchema = yup.object().shape({
+    username: yup
+        .string()
+        .min(3, 'Username must be at least four characters long')
+        .required('Username is required'),
+    password: yup
+        .string()
+        .min(6, 'Password must be at least six characters long')
+        .required('Password is required'),
+});
 
 const initialValues = {
     username: '',
@@ -37,8 +52,9 @@ const SignIn = () => {
       <Formik
         initialValues={initialValues}
         onSubmit={onSubmit}
+        validationSchema={validationSchema}
       >
-        {({ handleChange, handleSubmit, values }) => (
+        {({ handleChange, handleSubmit, values, errors, touched }) => (
           <>
             <View style={styles.input}>
             <TextInput
@@ -46,6 +62,9 @@ const SignIn = () => {
               onChangeText={handleChange("username")}
               value={values.username}
             />
+            {errors.username && touched.username && (
+                <Text style={styles.errorText}>{errors.username}</Text>
+            )}
             </View>
             <View style={styles.input}>
             <TextInput
@@ -54,6 +73,9 @@ const SignIn = () => {
               value={values.password}
               secureTextEntry
             />
+            {errors.password && touched.password && (
+                <Text style={styles.errorText}>{errors.password}</Text>
+              )}
             </View>
             <View style={styles.button}>
                 <TouchableOpacity onPress={handleSubmit}><Text>Sign in</Text></TouchableOpacity>
